fe3ca4825316bf37a4fd5849cb3fe4ecb9569e5e
Michael Sissons <michael_sissons@hotmail.com>
2019-10-26 17:11:58 -0800

    pain

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-var app = new Vue({
-  el: '#app',
+new Vue({
+  el: '#wrapper',
   data: {
-    message: 'Hello Vue!'
+    view: 'v-a',
+    
+  },
+  components: {
+    'v-a': {
+      template: "<div> <h2>This is a Home Page</h2> </div>"
+    },
+    'v-b': {
+      template: '<div><h2>Using CSS, Javascript and Vue.js</h2></div>'
+    },
+    'v-c': {
+      template: '<div><h2>Simple animations that engage the end user are now possible</h2></div>'
+    },
+    'v-d': {
+      template: '<div><h2>Making websites slick, professional and engaging.</h2></div>'
+    }
+  },
+  methods:{
+    turnA(){
+      if(this.view != 'v-a') this.view = 'v-a';
+    },
+    turnB(){
+      if(this.view != 'v-b') this.view = 'v-b';
+    },
+    turnC(){
+      if(this.view != 'v-c') this.view = 'v-c';
+    },
+    turnD(){
+      if(this.view != 'v-d') this.view = 'v-d';
+    }
   }
 })
