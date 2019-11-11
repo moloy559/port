25f03b82e82546d205d1a5a4898993f08b9a2150
Michael Sissons <michael_sissons@hotmail.com>
2019-11-10 17:24:58 -0800

    Some animations are working now.

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
+var introStrings = [
+  "first", "second", "third"
+]
+
+Vue.component('button-intro', {
+  data: function () {
+    return {
+      count: 0,
+      intro_btn_num: 0,
+      intro_content: introStrings[0],
+    }
+  },
+  template: ' <div class="button-intro"> \
+  <button class="txt-btn" v-on:click="introBtn">Click Me</button> \
+  <transition name="some-fade" mode="out-in" > \
+  <p  v-if="count==0" key="key1">This is a line of text. </p>\
+  <p  v-if="count==1" key="key2">Using CSS, Javascript and Vue.js </p>\
+  <p  v-if="count==2" key="key3">Simple animations that engage the end user are now possible</p>\
+  <p  v-if="count==3" key="key4">Making websites slick, professional and engaging.</p>\
+  </transition> \
+  </div> \
+  ',
+  methods:{
+    introBtn(){
+      if(this.count == 3) this.count = 0; else this.count++;
+      /*
+      if(this.intro_btn_num == introStrings.length - 1){
+        this.intro_btn_num = 0;
+      }else{
+        this.intro_btn_num++;
+      }
+      this.intro_content = introStrings[this.intro_btn_num]; */
+    }
+  }
+})
+
 new Vue({
   el: '#wrapper',
   data: {
     view: 'v-a',
+    
     page_num: 1,
     max_page_num: 1
   },
   components: {
     'v-a': {
-      template: "<div> <h2>This is a Home Page</h2> </div>"
+      template: '<div> \
+      <div class="frame"> \
+          <h2>Michael Sissons Portfolio</h2> \
+          <div class="txt-box"> \
+              <p>I am an application developer and designer that specializes in code. \
+                  This website was made to display my abilities to provide technical \
+                  solutions that enhance the design aspects within web and software applications. </p> \
+              <p> As a designer that specialises in code, my portfolio itself gets to be a part of my \
+                  portfolio.</p> \
+                  <p> Try clicking this button a few times.</p> \
+                  <button-intro></button-intro>\
+          </div> \
+      </div> \
+  </div>'
     },
     'v-b': {
-      template: '<div><h2>Using CSS, Javascript and Vue.js</h2> \
+      template: '<div><h2 class="title">Using CSS, Javascript and Vue.js</h2> \
       <div class="demoHolder">\
       <div class="videoWrapper">\
       <iframe  width="1903" height="768" src="https://www.youtube.com/embed/MqbYFTkyW9Y" \
@@ -54,5 +104,13 @@ new Vue({
     nextPage(){
       if(this.page_num != this.max_page_num) this.page_num++;
     },
+    introBtn(){
+      if(this.intro_btn_num == introStrings.length - 1){
+        this.intro_btn_num = 0;
+      }else{
+        this.intro_btn_num++;
+      }
+      this.intro_content = introStrings[this.intro_btn_num];
+    }
   }
 })
